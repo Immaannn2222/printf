dac2c370026c6ffc58040260d7e965475fff18a5
Immaannn2222 <imennaayari@gmail.com>
2020-03-17 00:44:28 +0100

    list it

---
+++ get_spec_char.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+  *get_spec_char - checks the characters
+  *@c: a charcter
+  *@p: the list
+  *Return: 1 or -1
+  */
+
+int get_spec_char(char c, va_list p)
+{
+spec_t spec[] = {
+{'c', print_char},
+{'s', print_string},
+{'%', print_percent},
+{'i', print_integer},
+{'d', print_d},
+{'0', NULL}
+};
+
+int i = 0;
+int len = 0;
+while (spec[i].x != '0')
+{
+if (spec[i].x == c)
+{
+len += spec[i].func(p);
+return (len);
+}
+i++;
+}
+return (-1);
+}
