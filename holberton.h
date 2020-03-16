5c29bd9814668ab74311774ad2362d64f0fc3a21
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-17 00:30:33 +0100

    headre file

---
+++ holberton.h
@@ -0,0 +1,24 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+#include <stddef.h>
+
+typedef struct specifiers
+{
+char x;
+int (*func)(va_list args);
+} spec_t;
+
+int get_spec_char(char c, va_list p);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list __attribute__((unused)) args);
+int print_integer(va_list args);
+int print_d(va_list args);
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+
+#endif
