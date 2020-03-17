a8dc3e26f206bd6f0b3b6ba9f970c9e52d17cc94
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-17 22:17:47 +0100

    print_oct.c

---
+++ print_oct.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  *print_oct - convert number to octal
+  *@args: argument
+  *Return: lenth
+  *
+  */
+
+int print_oct(va_list args)
+{
+	char octal[11];
+	unsigned int x = 0, n = 0;
+	int i = 0, oct = 0;
+
+	n = va_arg(args, int);
+if (n <= 1)
+{
+_putchar('-');
+n = -n;
+x += 1;
+	}
+if (n == 0)
+{
+_putchar('0');
+x = 1;
+}
+else
+{
+while (n >= 0)
+{
+		oct = n % 8;
+		octal[i++] = (oct + '0');
+		n = n / 8;
+		x++;
+}
+
+}
+while (i >= 0)
+{
+
+_putchar(octal[--i]);
+
+}
+
+return (x);
+}
