d3970e23991f2eebe77f088e04c5bdafd4aee465
Immaannn2222 <imennaayari@gmail.com>
2020-03-17 13:22:43 +0100

    fix

--- _printf.c
+++ _printf.c
@@ -15,29 +15,21 @@ int _printf(const char *format, ...)
 va_list args;
 unsigned int i = 0;
 int lenth = 0;
-
 va_start(args, format);
 if (format == NULL)
 return (-1);
-
 while (format[i] != '\0' && format)
 {
 lenth++;
 if (format[i] != '%')
 _putchar(format[i]);
-
 else
 {
 lenth += get_spec_char(format[i + 1], args) - 1;
 i = i + 1;
 }
 i++;
-
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> da8fe128471627f1ef50827b8298b5e5f6b21999
 va_end(args);
 return (lenth);
 }
