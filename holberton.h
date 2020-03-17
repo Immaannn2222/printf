00e4f57a47b21668b6c35d3eac1344dfa883e2e0
jlassi1 <khawlajlassi1990@gmail.com>
2020-03-17 21:30:39 +0100

    unsig_int.c

--- holberton.h
+++ holberton.h
@@ -19,6 +19,8 @@ int print_string(va_list args);
 int print_percent(va_list __attribute__((unused)) args);
 int print_integer(va_list args);
 int print_bin(va_list args);
+int print_unsig_int(va_list args);
+int print_oct(va_list o);
 
 int _putchar(char c);
 int _printf(const char *format, ...);
